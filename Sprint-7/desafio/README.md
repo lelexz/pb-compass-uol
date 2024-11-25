15233414818c3e87a2da5eb45a3298dc7937d933
letícia <lele-martins33@hotmail.com>
2024-11-25 12:41:27 -0300

    adicionando sprint 7

---
+++ Sprint-7/desafio/README.md
@@ -0,0 +1,59 @@
+##   Sprint 7 - Desafio 
+
+### Objetivo
+O objetivo deste desafio foi utilizar o AWS Lambda para fazer requisições à API do TMDB e enviar os dados obtidos diretamente para um bucket no S3.
+
+
+### Função Lambda e camada request
+Comecei criando uma função Lambda na AWS:
+
+![Clique aqui](../evidencias/funcao_lambda.jpg)
+
+Para conseguir fazer as requisições às APIs no AWS Lambda, precisei instalar a biblioteca requests no meu ambiente local, já que ela não vem integrada no serviço. Depois disso, criei uma camada personalizada no Lambda para incluir essa biblioteca, permitindo que o código consiga enviar e receber dados das APIs. 
+
+![Clique aqui](../evidencias/camadas.jpg)
+
+No código, comecei importando as bibliotecas necessárias, incluindo `boto3` para interação com o S3, `requests` para fazer as chamadas na API do TMDB e `datetime` para manipulação de datas Também criei dois arquivos separados por questão de segurança: `chave_api` e `chaves_aws`, para guardar a chave da API do TMDB e as credenciais da AWS.
+
+![Clique aqui](../evidencias/imports)
+
+Criei a função `fetch_tmdb_data` que faz as requisições para API. Ela recebe o endpoint, como o `discover/movie`, os parâmetros, como idioma e gênero, e a chave da API. A função monta a URL, faz a requisição, e se der tudo certo (status 200), retorna os dados da API em formato JSON. Caso contrário, ele mostra uma mensagem apontando o erro.
+
+![Clique aqui](../evidencias/fetch_tmdb_data.jpg)
+
+Depois, criei a função `fetch_crime_movies`, que configura os parâmetros necessários para buscar exclusivamente filmes do gênero crime (identificado pelo código 80). A função também define o idioma dos dados como português, aplica um filtro de datas de lançamento entre 1990 e 2000, e inclui um controle de paginação para facilitar o acesso a vários filmes de uma vez. Essa função usa a `fetch_tmdb_data` pra puxar os dados.
+
+![Clique aqui](../evidencias/fetch_crime_movies.jpg)
+
+Também desenvolvi a função `fetch_movie_details` para obter informações específicas de cada filme necessárias para minha análise. Ela faz duas requisições: uma para detalhes gerais do filme (título, orçamento) e outra para os créditos (nesse caso, o diretor). Além disso, ela lista os países de produção e substitui qualquer dado que esteja faltando por "Desconhecido".
+
+![Clique aqui](../evidencias/fetch_movies_details.jpg)
+
+Fiz a função `lambda_handler`, que é a parte principal do código. Ela começa definindo o número total de páginas a serem processadas e o valor inicial da página. Em seguida, entra em um loop onde chama a função `fetch_crime_movies` para buscar filmes de crime de cada página. Após obter os filmes de uma página, ela utiliza a função `fetch_movie_details` para buscar informações adicionais sobre cada filme.
+
+Todos os dados obtidos são armazenados em uma lista, que é dividida em grupos de até 100 filmes. Ao final, esses grupos são enviados para o S3 usando a função `upload_to_s3`, onde cada grupo de dados é transformado em um arquivo JSON e enviado para o bucket de armazenamento.
+
+![Clique aqui](../evidencias/lambda_handler1.jpg)
+![Clique aqui](../evidencias/lambda_handler2.jpg)
+
+Por fim, a função `upload_to_s3` envia os dados para o bucket no S3. Ela configura uma sessão com as credenciais da AWS armazenadas no arquivo `chaves_aws`, define o caminho do arquivo no S3 e utiliza o método `put_object` do cliente S3 para enviar os dados no formato JSON. Ao final, uma mensagem confirma o sucesso do upload, indicando o caminho completo no S3.
+
+![Clique aqui](../evidencias/upload_to_s3.jpg)
+
+
+### Executando
+Fiz um evento de teste e executei a função Lambda, que foi bem-sucedida. A função respondeu conforme esperado, processando os dados e realizando o upload para o S3 sem erros.
+
+![Clique aqui](../evidencias/paginas_carregadas.jpg)
+
+O bucket final ficou com a seguinte estrutura:
+
+![Clique aqui](../evidencias/objetos_bucket.jpg)
+
+
+### Sobre a análise
+Durante o desenvolvimento desse desafio, decidi focar em uma análise mais específica, comparando a nacionalidade dos diretores e os países de produção com o sucesso dos filmes. Para essa comparação, vou usar métricas como faturamento e popularidade. Essa abordagem me permitirá entender melhor como esses fatores influenciam o desempenho dos filmes.
+
+
+
+ff
---
+++ Sprint-7/desafio/lambda_function.py
@@ -0,0 +1,113 @@
+import boto3
+import chave_api
+import requests
+from datetime import datetime
+import chaves_aws
+import json
+
+#função de requisição à API
+def fetch_tmdb_data(api_key, endpoint, params):
+    #url da api com o endpoint específico
+    url = f"https://api.themoviedb.org/3/{endpoint}"
+    #adicionando a chave de API aos parâmetros
+    params["api_key"] = api_key
+
+    response = requests.get(url, params=params) #faz a requisição get
+
+    if response.status_code == 200: #verifica se a resposta foi bem sucedida
+        return response.json() #dados no formato json
+    else:
+        print(f"Falha: {response.status_code}") #se falhar, mostra o código do erro
+        return None
+
+
+def fetch_crime_movies(api_key, current_page=1):
+    params = {
+        "language": "pt-BR", #idioma de resposta
+        "with_genres": 80,  #gênero de crime
+        "page": current_page, #número da página
+        "release_date.gte": "1990-01-01",  #filtrando a partir de 1990
+        "release_date.lte": "2000-12-31",  #até 2000
+    }
+    return fetch_tmdb_data(api_key, "discover/movie", params)
+
+
+def fetch_movie_details(api_key, movie_id):
+    #buscando detalhes do filme
+    details = fetch_tmdb_data(api_key, f"movie/{movie_id}", {})
+    credits = fetch_tmdb_data(api_key, f"movie/{movie_id}/credits", {})
+
+    if not details or not credits: #verifica se as requisições falharam
+        return None
+
+    return {
+        "id": movie_id,
+        "imdb_id": details.get("imdb_id", "Desconhecido"),
+        "title": details.get("title"),
+        "director": next(
+        (person["name"] for person in credits["crew"] if person["job"] == "Director"),
+        "Desconhecido"),  #extrai o nome do diretor da lista de créditos
+        "duration": details.get("runtime", "Desconhecido"),
+        "budget": details.get("budget", "Desconhecido"),
+        "revenue": details.get("revenue", "Desconhecido"),
+        "release_date": details.get("release_date", "Desconhecido"),
+        "popularity": details.get("popularity", "Desconhecido"),
+        "vote_average": details.get("vote_average", "Desconhecido"),  
+        "original_language": details.get("original_language", "Desconhecido"),  
+        "production_countries": [country["name"] for country in details.get("production_countries", [])] or "Desconhecido", #extrai os países de produção
+    }
+
+#função para buscar os filmes e salvar no S3
+def lambda_handler(event, context):
+    api_key = chave_api.chave
+    bucket = "bkt-desafio"
+    total_pages = 10 #número de páginas para buscar
+    current_page = 1 #começando na primeira página
+    tmdb_data = [] #lista para armazenar os dados dos filmes
+
+    #loop para carregar todas as páginas
+    while current_page <= total_pages:
+        tmdb_response = fetch_crime_movies(api_key, current_page)
+
+        if tmdb_response:
+            print(f"Página {current_page} carregada com sucesso.")
+            #itera cada filme que veio na resposta da api
+            for movie in tmdb_response["results"]:
+                #busca mais detalhes do filme utilizando o id
+                movie_details = fetch_movie_details(api_key, movie["id"])
+                #se encontrou os detalhes, joga na lista
+                if movie_details:
+                    tmdb_data.append(movie_details)
+
+            if current_page >= total_pages:
+                break  #sai do loop quando todas as páginas forem processadas
+
+            current_page += 1
+        else:
+            print("Falha ao buscar dados da API.")
+            break
+    #dividindo os dados em grupos de 100 filmes
+    group_data = [tmdb_data[i : i + 100] for i in range(0, len(tmdb_data), 100)]
+    current_date = datetime.now().strftime("%Y/%m/%d")
+
+    #envia os dados para o S3, um grupo de cada vez 
+    for index, data_group in enumerate(group_data, start=1):
+        upload_to_s3(json.dumps(data_group), bucket, f"tmdb_data_{index}", current_date)
+
+#função para fazer upload dos dados no S3
+def upload_to_s3(data, bucket, tipo, current_date):
+    #caminho
+    s3_key = f"Raw/tmdb/json/{current_date}/{tipo.lower()}.json"
+
+    session = boto3.Session(
+        aws_access_key_id=chaves_aws.access_key,
+        aws_secret_access_key=chaves_aws.secret_key,
+        aws_session_token=chaves_aws.session_token,
+        region_name="us-east-1"
+    )
+    s3 = session.client("s3")
+
+    s3.put_object(Body=data, Bucket=bucket, Key=s3_key)
+    print(f"Dados carregados para S3: s3://{bucket}/{s3_key}")
+    
+    ff
